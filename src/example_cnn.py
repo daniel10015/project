Add relative timing calculations and auto-unit conversion with a stress test. Found bug with failing assertion.

src/example_cnn.py
@@ -32,13 +32,6 @@ def do_something():
 def do_profile():
   profile = profiler(do_something, ('MEMORY',))
   profile()
-  return
-  profile.visualize('MEMORY')
-
-  profile_info = profile.spill()
-  for metric_type, metric_out in profile_info.items():
-    info = f'{metric_type} => {metric_out}'
-    print(info)
 
 def do_torchprofile():
   with torch.profiler.profile(activities=[torch.profiler.ProfilerActivity.CPU, torch.profiler.ProfilerActivity.CUDA], record_shapes=True) as prof:
@@ -57,3 +50,12 @@ torch_time = benchmark.benchmark_ns(do_torchprofile)
 print(f'regular time took: {regular_time}ns\n'
       f'profile time took: {profile_time}ns\n'
       f'torch time took: {torch_time}ns')
+
+profile = profiler(do_something, ('MEMORY',))
+profile()
+profile.visualize('MEMORY')
+
+profile_info = profile.spill()
+for metric_type, metric_out in profile_info.items():
+  info = f'{metric_type} => {metric_out}'
+  print(info)

src/profiler/metric_callback.py
@@ -13,12 +13,12 @@ class metric_callback:
   
   def memory(self, activity) -> str:
     isAlloc = activity.memory_operation_type == cupti.ActivityMemoryOperationType.ALLOCATION
-    opTime = util.ns_to_s(activity.timestamp) - self.start_time_s
+    opTime = util.ns_to_s(activity.timestamp)
     opType = 'malloc' if isAlloc else 'free'
     size = activity.bytes
     addr = activity.address
     if self.memory_info.get(addr):
-      assert (not isAlloc) and self.memory_info[addr]['size'] == size, "activity should be a free but it's not"
+      assert (not isAlloc) and self.memory_info[addr]['size'] == size, f"activity ({activity.memory_operation_type}) should be a free but it's not"
       self.memory_info[addr]['end'] = opTime
     else:
       self.memory_info[addr] = {'size':size, 'start':opTime}
@@ -44,6 +44,8 @@ class metric_callback:
     # Compute cumulative utilization over time
     times, sizes = zip(*events)
     times = np.array(times)
+    times = times - np.min(times) # offset to 0
+    times, units = util.scale_time_units(times_ns=times)
     deltas = np.array(sizes)
     utilization = np.cumsum(deltas)
 
@@ -54,7 +56,7 @@ class metric_callback:
     plt.figure(figsize=(8, 4))
     plt.step(times, utilization_MB, where="post", lw=2)
     plt.fill_between(times, utilization_MB, step="post", alpha=0.3)
-    plt.xlabel("Time (s)")
+    plt.xlabel(f"Time ({units})")
     plt.ylabel("Memory (MB)")
     plt.title('Memory Utilization Over Time')
     plt.grid(True, linestyle="--", alpha=0.5)